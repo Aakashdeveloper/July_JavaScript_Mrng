f7b87f74888038d8d046e620ce71a0aa4967659f
aakashDeveloper <ahanda205@gmail.com>
2020-06-29 07:47:47 +0530

    a

---
+++ four.js
@@ -0,0 +1,121 @@
+var name= "alvin"
+undefined
+name.toUpperCase()
+"ALVIN"
+name.toLowerCase()
+"alvin"
+a.length
+undefined
+name[0]
+"a"
+name[1]
+"l"
+var name= "   alvin     "
+undefined
+a.length
+undefined
+name.length
+13
+
+var name= "   alvin     "
+undefined
+name.trim()
+"alvin"
+var name= "   alvin .  "
+undefined
+name.trim()
+"alvin ."
+
+
+var name = "aLViN"
+undefined
+
+name.charAt(0)
+"a"
+name.charAt(1)
+"L"
+name.slice(1)
+"LViN"
+name.slice(2)
+"ViN"
+
+var name = "aLViN"
+undefined
+name.charAt
+ƒ charAt() { [native code] }
+name.charAt(0)
+"a"
+name.charAt(1)
+"L"
+name.slice(1)
+"LViN"
+name.slice(2)
+"ViN"
+name.slice(2,4)
+"Vi"
+name.slice(1,4)
+"LVi"
+name.slice(1,5)
+"LViN"
+
+name.slice(a)
+a here is number of word to remove
+name.slice(start,end)
+
+var name = "aLViN"
+undefined
+name.charAt(0).toUpperCase()
+"A"
+name.slice(1).toLowerCase()
+"lvin"
+name.charAt(0).toUpperCase()+name.slice(1).toLowerCase()
+"Alvin"
+
+var a  = "  I am doing Javascript  "
+
+var a  = "  I am doing Javascript  "
+undefined
+a.trim().toUpperCase()
+"I AM DOING JAVASCRIPT"
+var a  = "I am doing Javascript"
+undefined
+a.replace('Javascript','JS')
+"I am doing JS"
+var a  = "Javascript I am doing Javascript"
+undefined
+a.replace('Javascript','JS')
+"JS I am doing Javascript"
+a.replace(/Javascript/g,'JS')
+"JS I am doing JS"
+
+var a  = "  I am doing Javascript  "
+undefined
+a.trim().toUpperCase()
+"I AM DOING JAVASCRIPT"
+var a  = "I am doing Javascript"
+undefined
+a.replace('Javascript','JS')
+"I am doing JS"
+var a  = "Javascript I am doing Javascript"
+undefined
+a.replace('Javascript','JS')
+"JS I am doing Javascript"
+a.replace(/Javascript/g,'JS')
+"JS I am doing JS"
+
+
+slice
+substring
+substr
+Math
+
+var a = 74678         
+undefined
+var a = 74678         
+undefined
+a+1
+74679
+var b = " dgf . "
+undefined
+b+1
+" dgf . 1"
---
+++ third.js
@@ -0,0 +1,43 @@
+= assignment
+== comapre the data
+=== compare data and datatype
+
+var a = 10
+undefined
+var b = "10"
+undefined
+typeof(a)
+"number"
+typeof(b)
+"string"
+a == b
+true
+a === b
+false
+//////////////
+var a = true
+undefined
+!a
+false
+var b = !a
+undefined
+b
+false
+!b
+true
+!!b
+false
+!!!b
+true
+!!!!b
+false
+var a = 10
+undefined
+var b = 20
+undefined
+a !=b
+true
+a !== b
+true
+a !=== b
+VM844:1 Uncaught SyntaxError: Unexpected token '='
