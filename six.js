c38b01bd4c0b4261790f93ad951ea6f1142a0042
aakashDeveloper <ahanda205@gmail.com>
2020-07-01 08:40:58 +0530

    a

---
+++ six.js
@@ -0,0 +1,25 @@
+//Persistent
+localStorage.setItem('__TknTkn__','dfb43gr4drs43rb')
+undefined
+localStorage.getItem('__TknTkn__')
+"dfb43gr4drs43rb"
+localStorage.removeItem('__TknTkn__')
+undefined
+localStorage.getItem('__TknTkn__')
+null
+
+////Remove when you close the tab
+sessionStorage.setItem('abcd___','dvvwd43v43er4e')
+undefined
+sessionStorage.getItem('abcd___')
+"dvvwd43v43er4e"
+sessionStorage.removeItem('abcd___')
+undefined
+
+///////Cookies///////
+It save wrt to webiste
+///////////
+document.cookie = "mycookies=jnvjdnvkjbvj"
+"mycookies=jnvjdnvkjbvj"
+document.cookie = "mycookies1=jnvjdnvkjbvj; expires = 2 Jul 2020"
+"mycookies=jnvjdnvkjbvj"
