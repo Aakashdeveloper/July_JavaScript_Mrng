a2776caa60a9d305618a2456f5f54e9d4591d5e6
aakashdeveloper <ahanda205@gmail.com>
2020-07-21 07:44:52 +0530

    A

---
+++ calc.js
@@ -0,0 +1,23 @@
+  /*function add(){
+                var a = document.getElementById('first').value;
+                var b = document.getElementById('second').value;
+                var out = parseFloat(a)+parseFloat(b);
+                document.getElementById('output').innerText=out
+            }
+            function sub(){
+                var a = document.getElementById('first').value;
+                var b = document.getElementById('second').value;
+                var out = parseFloat(a)-parseFloat(b);
+                document.getElementById('output').innerText=out
+            }*/
+            function calc(opt){
+                var a = document.getElementById('first').value;
+                var b = document.getElementById('second').value;
+                var out;
+                if(opt=='add'){
+                    out = parseFloat(a)+parseFloat(b);
+                }else{
+                    out = parseFloat(a)-parseFloat(b);
+                }
+                document.getElementById('output').innerText=out
+            }
