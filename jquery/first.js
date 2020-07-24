d9bf6c0a1aea913eff73e142a483c44ec912f6f7
aakashdeveloper <ahanda205@gmail.com>
2020-07-24 07:33:48 +0530

    A

---
+++ jquery/first.js
@@ -0,0 +1,8 @@
+$('h1')
+m.fn.init [h1, prevObject: m.fn.init(1), context: document, selector: "h1"]
+$('h1')[0].innerText
+"I'm AAKASH HANDA"
+$('.classname')
+m.fn.init [prevObject: m.fn.init(1), context: document, selector: ".classname"]
+
+$('h1')[0].innerText
